f5958c235a53d3b990e731a88e327a4716bb5bd0
qingwei.li <cinwell.li@gmail.com>
2016-05-31 13:15:11 +0800

    fix style

--- example/pages/tabbar.vue
+++ example/pages/tabbar.vue
@@ -1,27 +1,29 @@
 <template>
   <div class="page-tabbar">
-    <div class="page-title">Tabbar</div>
-    <div>
-      <mt-cell class="page-part" title="当前选中" :value="selected"></mt-cell>
-    </div>
+    <div class="page-wrap">
+      <div class="page-title">Tabbar</div>
+      <div>
+        <mt-cell class="page-part" title="当前选中" :value="selected"></mt-cell>
+      </div>
 
-    <mt-panel style="page-tabbar-panel" :active.sync="selected">
-      <mt-panel-item id="外卖">
-        <mt-cell v-for="n in 10" :title="'餐厅 ' + $index"></mt-cell>
-      </mt-panel-item>
-      <mt-panel-item id="订单">
-        <mt-cell v-for="n in 5" :title="'订单 ' + $index"></mt-cell>
-      </mt-panel-item>
-      <mt-panel-item id="发现">
-        <mt-cell v-for="n in 7" :title="'发现 ' + $index"></mt-cell>
-      </mt-panel-item>
-      <mt-panel-item id="我的">
-        <div class="page-part">
-          <mt-cell v-for="n in 12" :title="'我的 ' + $index"></mt-cell>
-        </div>
-        <mt-button v-link="'/'" style="margin: 0 10px;" type="danger" size="large">退出</mt-button>
-      </mt-panel-item>
-    </mt-panel>
+      <mt-panel style="page-tabbar-panel" :active.sync="selected">
+        <mt-panel-item id="外卖">
+          <mt-cell v-for="n in 10" :title="'餐厅 ' + $index"></mt-cell>
+        </mt-panel-item>
+        <mt-panel-item id="订单">
+          <mt-cell v-for="n in 5" :title="'订单 ' + $index"></mt-cell>
+        </mt-panel-item>
+        <mt-panel-item id="发现">
+          <mt-cell v-for="n in 7" :title="'发现 ' + $index"></mt-cell>
+        </mt-panel-item>
+        <mt-panel-item id="我的">
+          <div class="page-part">
+            <mt-cell v-for="n in 12" :title="'我的 ' + $index"></mt-cell>
+          </div>
+          <mt-button v-link="'/'" style="margin: 0 10px;" type="danger" size="large">退出</mt-button>
+        </mt-panel-item>
+      </mt-panel>
+    </div>
 
     <mt-tabbar :selected.sync="selected">
       <mt-tab-item id="外卖">
@@ -56,7 +58,18 @@ export default {
 </script>
 
 <style>
+  .page-tabbar {
+    overflow: hidden;
+    height: 100vh;
+  }
+
   .page-tabbar-panel {
     overflow: auto;
+  }
+
+  .page-wrap {
+    overflow: auto;
+    height: 100%;
+    padding-bottom: 100px;
   }
 </style>

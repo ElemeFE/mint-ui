74a151c622b16c06d6edb58ce2637d8ea347621a
Leopoldthecoder <Leopoldthecuber@gmail.com>
2016-06-03 12:17:47 +0800

    update picker

--- example/pages/picker.vue
+++ example/pages/picker.vue
@@ -2,17 +2,17 @@
   <div class="page-picker">
     <h1 class="page-title">Picker</h1>
     <div class="page-picker-wrapper">
-      <mt-picker :slots="yearSlot" @change="onYearChange" :visible-item-count="3" :show-toolbar="false"></mt-picker>
+      <mt-picker :slots="yearSlot" @change="onYearChange" :visible-item-count="3"></mt-picker>
     </div>
     <p class="page-picker-desc">出生年份: {{ year }}</p>
 
     <div class="page-picker-wrapper">
-      <mt-picker :slots="dateSlots" @change="onDateChange" :visible-item-count="3" :show-toolbar="false"></mt-picker>
+      <mt-picker :slots="dateSlots" @change="onDateChange" :visible-item-count="3"></mt-picker>
     </div>
     <p class="page-picker-desc">在校时间: {{ dateStart }} 至 {{ dateEnd }}</p>
 
     <div class="page-picker-wrapper">
-      <mt-picker :slots="addressSlots" @change="onAddressChange" :visible-item-count="5" :show-toolbar="false"></mt-picker>
+      <mt-picker :slots="addressSlots" @change="onAddressChange" :visible-item-count="5"></mt-picker>
     </div>
     <p class="page-picker-desc">地址: {{ addressProvince }} {{ addressCity }}</p>
   </div>

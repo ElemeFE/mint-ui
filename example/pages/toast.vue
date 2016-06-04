b62d23a2ab5b046591400dd52a91de5ebb4ae6cf
Leopoldthecoder <Leopoldthecuber@gmail.com>
2016-06-04 08:13:43 +0800

    update toast example

--- example/pages/toast.vue
+++ example/pages/toast.vue
@@ -4,6 +4,7 @@
     <div class="page-toast-wrapper">
       <mt-button @click="openToast" size="large">点击弹出 Toast</mt-button>
       <mt-button @click="openToastWithIcon" size="large">点击弹出带有 icon 的 Toast</mt-button>
+      <mt-button @click="openBottomToast" size="large">自定义 Toast 位置</mt-button>
     </div>
   </div>
 </template>
@@ -16,7 +17,7 @@
         position: absolute 50% * * *;
         width: 100%;
         transform: translateY(-50%);
-        a:first-child {
+        a:not(:last-child) {
           margin-bottom: 20px;
         }
       }
@@ -38,6 +39,13 @@
         Toast({
           message: '操作成功',
           iconClass: 'mintui mintui-success'
+        });
+      },
+
+      openBottomToast() {
+        Toast({
+          message: '提示信息',
+          position: 'bottom'
         });
       }
     }

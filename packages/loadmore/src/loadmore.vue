82b091459a5c9caf42ba6fdb3c3a295486dbfd60
杨奕 <Leopoldthecoder@users.noreply.github.com>
2016-06-14 18:11:41 +0800

    fix a loadmore bug (#92)
    * fix a bug that  pull-up fails when loadmore's parent node has a padding-bottom
    
    * Publish
    
     - mint-loadmore@0.1.2
     - mint-spinner@0.2.1
    
    * 0.1.12

--- packages/loadmore/src/loadmore.vue
+++ packages/loadmore/src/loadmore.vue
@@ -2,14 +2,14 @@
   <div class="mint-loadmore">
     <div class="mint-loadmore-content" :class="{ 'is-dropped': topDropped || bottomDropped}" :style="{ 'transform': 'translate3d(0, ' + translate + 'px, 0)' }" v-el:loadmore-content>
       <slot name="top">
-        <div class="mint-loadmore-top">
+        <div class="mint-loadmore-top" v-if="topMethod">
           <spinner v-if="topStatus === 'loading'" class="mint-loadmore-spinner" :size="20" type="fading-circle"></spinner>
           <span class="mint-loadmore-text">{{ topText }}</span>
         </div>
       </slot>
       <slot></slot>
       <slot name="bottom">
-        <div class="mint-loadmore-bottom">
+        <div class="mint-loadmore-bottom" v-if="bottomMethod">
           <spinner v-if="bottomStatus === 'loading'" class="mint-loadmore-spinner" :size="20" type="fading-circle"></spinner>
           <span class="mint-loadmore-text">{{ bottomText }}</span>
         </div>
@@ -259,7 +259,7 @@
         if (this.scrollEventTarget === window) {
           return document.body.scrollTop + document.documentElement.clientHeight === document.body.scrollHeight;
         } else {
-          return this.$el.getBoundingClientRect().bottom === this.scrollEventTarget.getBoundingClientRect().bottom;
+          return this.$el.getBoundingClientRect().bottom <= this.scrollEventTarget.getBoundingClientRect().bottom;
         }
       },
 

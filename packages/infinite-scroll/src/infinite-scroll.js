6807ed424da1a35dbe0f812379c485db84cfd73e
qingwei.li <cinwell.li@gmail.com>
2016-07-21 10:48:16 +0800

    fix infinitescroll entry

--- packages/infinite-scroll/src/infinite-scroll.js
+++ packages/infinite-scroll/src/infinite-scroll.js
@@ -1,5 +1,6 @@
-import infiniteScroll from 'vue-infinite-scroll';
+import { install, infiniteScroll } from 'vue-infinite-scroll';
 import 'main/style/empty.css';
 
 infiniteScroll.name = 'infinite-scroll';
+infiniteScroll.install = install;
 module.exports = infiniteScroll;

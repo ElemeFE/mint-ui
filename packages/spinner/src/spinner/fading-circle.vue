7e5d6cec98e942b468f50439e32d02d758764ed9
qingwei.li <cinwell.li@gmail.com>
2016-08-18 12:25:18 +0800

    Update spinner

--- packages/spinner/src/spinner/fading-circle.vue
+++ packages/spinner/src/spinner/fading-circle.vue
@@ -3,19 +3,7 @@
       width: spinnerSize,
       height: spinnerSize
     }">
-    <div class="is-circle1 kebab-spinner-fading-circle-circle"></div>
-    <div class="is-circle2 kebab-spinner-fading-circle-circle"></div>
-    <div class="is-circle3 kebab-spinner-fading-circle-circle"></div>
-    <div class="is-circle4 kebab-spinner-fading-circle-circle"></div>
-    <div class="is-circle5 kebab-spinner-fading-circle-circle"></div>
-    <div class="is-circle6 kebab-spinner-fading-circle-circle"></div>
-    <div class="is-circle7 kebab-spinner-fading-circle-circle"></div>
-    <div class="is-circle8 kebab-spinner-fading-circle-circle"></div>
-    <div class="is-circle9 kebab-spinner-fading-circle-circle"></div>
-    <div class="is-circle10 kebab-spinner-fading-circle-circle"></div>
-    <div class="is-circle11 kebab-spinner-fading-circle-circle"></div>
-    <div class="is-circle12 kebab-spinner-fading-circle-circle"></div>
-  </div>
+    <div v-for="n in 12" :class="['is-circle' + (n + 1)]" class="kebab-spinner-fading-circle-circle"></div>
 </template>
 
 <script>

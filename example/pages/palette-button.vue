c1ce7ff7430e5ec85d4072dede775c18105653f9
Tiger <cia.fbi.007@hotmail.com>
2016-10-26 11:38:02 +0800

    完成基本功能

--- example/pages/palette-button.vue
+++ example/pages/palette-button.vue
@@ -1,13 +1,45 @@
 <template>
-    <mt-palette-button></mt-palette-button>
+  <div>
+    {{expanded}}
+    <mt-palette-button content="+" @click="log(0)" direction="lt" class="pb" :radius="100" :expand="expanded">
+      <div class="my-icon-button indexicon icon-pull-up" @touchstart="log(1)"></div>
+      <div class="my-icon-button indexicon icon-infinite-scroll" @touchstart="log(2)"></div>
+      <div class="my-icon-button indexicon icon-popup" @touchstart="log(3)"></div>
+      <div class="my-icon-button indexicon icon-popup" @touchstart="log(4)"></div>
+      <div class="my-icon-button indexicon icon-popup" @touchstart="log(5)"></div>
+    </mt-palette-button>
+  </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      selected: '1'
-    };
-  }
-};
+  export default {
+    methods: {
+      log(val) {
+        console.log(val);
+      }
+    },
+    data: function() {
+      return {
+        expanded: false
+      };
+    }
+  };
 </script>
+
+<style scoped>
+  .pb{
+    width: 60px;
+    height: 60px;
+    right: 50px;
+    line-height: 60px;
+  }
+  .my-icon-button{
+    width:30px;
+    height:30px;
+    border-radius:50%;
+    background-color:red;
+    line-height:30px;
+    text-align:center;
+  }
+</style>
+ 
--- packages/palette-button/src/palette-button.vue
+++ packages/palette-button/src/palette-button.vue
@@ -1,3 +1,134 @@
 <template>
-    <div>hello</div>
+	<div class="palette-button" :class="{ expand: expand, 'palette-button-active': transforming }" @animationend="onMainAnimationEnd">
+    <div class="sub-button-container">
+      <slot></slot>
+    </div>
+    <div @touchstart="onMainClick" class="main-button">
+      {{content}}
+    </div>
+  </div>
 </template>
+
+<script>
+  export default {
+    name: 'mt-palette-button',
+    data: function() {
+      return {
+        transforming: false     // 是否正在执行动画
+      };
+    },
+    props: {
+      content: {
+        type: String,
+        default: '+'
+      },
+      offset: {
+        type: Number,           // 扇面偏移角，默认是四分之π，配合默认方向lt
+        default: Math.PI / 4
+      },
+      direction: {
+        type: String,
+        default: 'lt'           // lt t rt this.radius rb b lb l 取值有8个方向，左上、上、右上、右、右下、下、左下、左，默认为左上
+      },
+      radius: {
+        type: Number,
+        default: 90
+      },
+      expand: {
+        type: Boolean,
+        default: false           // 是否已经展开子按钮
+      }
+    },
+    methods: {
+      onMainClick(event) {
+        if (!this.transforming) {
+          this.expand = this.expand !== true;
+          this.transforming = true;
+          this.$emit('click', event);
+        }
+      },
+      onMainAnimationEnd(event) {
+        this.transforming = false;
+      }
+    },
+    mounted() {
+      this.slotChildren = [];
+      for (let i = 0; i < this.$slots.default.length; i++) {
+        if (this.$slots.default[i].elm.nodeType !== 3) {
+          this.slotChildren.push(this.$slots.default[i]);
+        }
+      }
+
+      let css = '';
+      var direction_arc = Math.PI * (3 + Math.max(['lt', 't', 'rt', 'r', 'rb', 'b', 'lb', 'l'].indexOf(this.direction), 0)) / 4;
+      for (let i = 0; i < this.slotChildren.length; i++) {
+        var arc = (Math.PI - this.offset * 2) / (this.slotChildren.length - 1) * i + this.offset + direction_arc;
+        var x = Math.cos(arc) * this.radius;
+        var y = Math.sin(arc) * this.radius;
+        var item_css = '.expand .palette-button-sub-' + i + '{transform:translate(' + x + 'px,' + y + 'px) rotate(720deg);transition-delay:' + 0.03 * i + 's}';
+        css += item_css;
+
+        this.slotChildren[i].elm.className += (' palette-button-sub-' + i);
+      }
+
+      this.styleNode = document.createElement('style');
+      this.styleNode.type = 'text/css';
+      this.styleNode.rel = 'stylesheet';
+      this.styleNode.title = 'palette button style';
+      this.styleNode.appendChild(document.createTextNode(css));
+      document.getElementsByTagName('head')[0].appendChild(this.styleNode);
+    },
+    destroyed() {
+      if (this.styleNode) {
+        this.styleNode.parentNode.removeChild(this.styleNode);
+      }
+    }
+  };
+</script>
+
+<style scoped>
+  .palette-button{
+    position:absolute;
+    border-radius:50%;
+    right:20px;
+    bottom:20px;
+    width: 56px;
+    height:56px;
+    line-height:56px;
+    text-align:center;
+    transition:transform .1s ease-in-out;
+  }
+
+  .main-button{
+    position:absolute;
+    top:0;
+    left:0;
+    width:100%;
+    height:100%;
+    border-radius:50%;
+    background-color:blue;
+    color:#FFF;
+    font-size:2em;
+  }
+  .palette-button-active{
+    animation: zoom 0.5s ease-in-out;
+  }
+  .sub-button-container>*{
+    position:absolute;
+    top:15px;
+    left:15px;
+    width:25px;
+    height:25px;
+    transition: transform .3s ease-in-out;
+  }
+
+  @keyframes zoom{
+    0% {transform:scale(1)}
+    10% {transform:scale(1.1)}
+    30% {transform:scale(0.9)}
+    50% {transform:scale(1.05)}
+    70% {transform:scale(0.95)}
+    90% {transform:scale(1.01)}
+    100% {transform:scale(1)}
+  }
+</style>

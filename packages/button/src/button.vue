49770730530a256f4e2ba94dd35e6d7e163d96ec
qingwei.li <cinwell.li@gmail.com>
2016-10-27 21:27:29 +0800

    Button: fix disabled, fixed #366

--- packages/button/src/button.vue
+++ packages/button/src/button.vue
@@ -5,7 +5,7 @@
       'is-disabled': disabled,
       'is-plain': plain
     }]"
-    @touchstart="handleClick">
+    :disabled="disabled">
     <span class="mint-button-icon" v-if="icon || $slots.icon">
       <slot name="icon">
         <i v-if="icon" class="mintui" :class="'mintui-' + icon"></i>
@@ -62,15 +62,6 @@ export default {
           'normal',
           'large'
         ].indexOf(value) > -1;
-      }
-    }
-  },
-
-  methods: {
-    handleClick($event) {
-      if (this.disabled) {
-        $event.preventDefault();
-        $event.stopPropagation();
       }
     }
   }

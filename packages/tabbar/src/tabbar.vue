d3d27037ef5fe039605cb1b10b7f6e25e8551dd1
Thinking Song <123wuming_123@163.com>
2016-10-27 17:01:09 +0800

    update tabbar doc error (#369)
    * update doc error
    
    * update tabbar doc
    
    * update tabbar doc

--- packages/tabbar/src/tabbar.vue
+++ packages/tabbar/src/tabbar.vue
@@ -12,17 +12,17 @@
  * @module components/tabbar
  * @desc 底部 tab，依赖 tab-item
  * @param {boolean} [fixed=false] - 固定底部
- * @param {*} selected - 返回 item component 传入的 id
+ * @param {*} value - 返回 item component 传入的 id
  *
  * @example
- * <mt-tabbar :selected.sync="selected">
+ * <mt-tabbar v-model="selected">
  *   <mt-tab-item id="订单">
  *     <img slot="icon" src="http://placehold.it/100x100">
  *     <span slot="label">订单</span>
  *   </mt-tab-item>
  * </mt-tabbar>
  *
- * <mt-tabbar :selected.sync="selected" fixed>
+ * <mt-tabbar v-model="selected" fixed>
  *   <mt-tab-item :id="['传入数组', '也是可以的']">
  *     <img slot="icon" src="http://placehold.it/100x100">
  *     <span slot="label">订单</span>

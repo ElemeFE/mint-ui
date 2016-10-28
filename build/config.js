e854cd07dbd40799f3aff94441c98db22e0509c8
qingwei.li <cinwell.li@gmail.com>
2016-10-28 12:06:37 +0800

    Update config

--- build/config.js
+++ build/config.js
@@ -1,12 +1,16 @@
 var Components = require('../components.json');
 var path = require('path');
+var dependencies = require('../package.json').dependencies;
 var externals = {};
 
 Object.keys(Components).forEach(function(key) {
   externals[`mint-ui/packages/${key}/index.js`] = `mint-ui/lib/${key}`;
   externals[`mint-ui/packages/${key}/style.css`] = `mint-ui/lib/${key}/style.css`;
 });
-
+Object.keys(dependencies).forEach(function(key) {
+  externals[key] = key;
+});
+g
 exports.externals = Object.assign({
   vue: {
     root: 'Vue',

1906116a6257cdde321f100f7195ad639dcb3066
Leopoldthecoder <Leopoldthecuber@gmail.com>
2016-12-03 14:49:13 +0800

    Picker: improve value key

--- packages/picker/src/picker.vue
+++ packages/picker/src/picker.vue
@@ -2,7 +2,7 @@
   <div class="picker" :class="{ 'picker-3d': rotateEffect }">
     <div class="picker-toolbar" v-if="showToolbar"><slot></slot></div>
     <div class="picker-items">
-      <picker-slot v-for="slot in slots" :values="slot.values || []" :text-align="slot.textAlign || 'center'" :visible-item-count="visibleItemCount" :class-name="slot.className" :flex="slot.flex" v-model="values[slot.valueIndex]" :rotate-effect="rotateEffect" :divider="slot.divider" :content="slot.content"></picker-slot>
+      <picker-slot v-for="slot in slots" :valueKey="valueKey" :values="slot.values || []" :text-align="slot.textAlign || 'center'" :visible-item-count="visibleItemCount" :class-name="slot.className" :flex="slot.flex" v-model="values[slot.valueIndex]" :rotate-effect="rotateEffect" :divider="slot.divider" :content="slot.content"></picker-slot>
       <div class="picker-center-highlight"></div>
     </div>
   </div>
@@ -82,6 +82,7 @@
         type: Number,
         default: 5
       },
+      valueKey: String,
       rotateEffect: {
         type: Boolean,
         default: false

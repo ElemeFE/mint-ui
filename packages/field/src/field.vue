ca3e382c77f0b06f432a177c2678c83ba9472a56
yuchonghua <yuchonghua@163.com>
2017-01-03 20:06:36 +0800

    add ssr support
    add ssr support

--- packages/field/src/field.vue
+++ packages/field/src/field.vue
@@ -50,7 +50,8 @@
 <script>
 import XCell from 'mint-ui/packages/cell/index.js';
 import Clickoutside from 'mint-ui/src/utils/clickoutside';
-if (process.env.NODE_ENV === 'component') {
+import Vue from 'vue';
+if (!Vue.prototype.$isServer && process.env.NODE_ENV === 'component') {
   require('mint-ui/packages/cell/style.css');
 }
 

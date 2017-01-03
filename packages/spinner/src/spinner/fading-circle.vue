f100b166b962bd983e8085665746686ac3dc9b44
yuchonghua <yuchonghua@163.com>
2017-01-03 19:02:19 +0800

    last add this.$isServer

--- packages/spinner/src/spinner/fading-circle.vue
+++ packages/spinner/src/spinner/fading-circle.vue
@@ -16,7 +16,7 @@
     mixins: [common],
 
     created() {
-      if (typeof document === 'undefined') return;
+      if (this.$isServer) return;
       this.styleNode = document.createElement('style');
       const css = `.circle-color-${this._uid} > div::before { background-color: ${this.spinnerColor}; }`;
 

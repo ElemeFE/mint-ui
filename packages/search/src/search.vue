3b94cfa7f9a68b79d1f55c902adbfa4585d779f5
DeepKolos <k1206521365k@126.com>
2017-11-13 17:03:40 +0800

    完成search的优化~

--- packages/search/src/search.vue
+++ packages/search/src/search.vue
@@ -1,24 +1,32 @@
 <template>
   <div class="mint-search">
-    <div class="mint-searchbar">
-      <div class="mint-searchbar-inner">
-        <i class="mintui mintui-search"></i>
-        <input
-        ref="input"
-        @click="visible = true"
-        type="search"
-        v-model="currentValue"
-        :placeholder="placeholder"
-        class="mint-searchbar-core">
+    <div class="mint-searchbar" :data-expand="expand" :data-has-value="hasValue" :data-status="status" :data-fixed-cancel="fixedCancelBtn" :data-lang="lang">
+      <div class="input-container">
+        <span class="placeholder-container">
+          <i class="mintui mintui-search search-icon"></i>
+          <span class="placeholder">{{placeholder}}</span>
+        </span>
+        <div class="text-input-container">
+          <input 
+            ref="input"
+            type="text"
+            class="text-input"
+            @focus="focus"
+            @blur="_blur"
+            @input="_input"
+            v-model="currentValue"
+          >
+          <div class="btn-clear" @click="_clear" v-show="showCancelBtn"></div>
+        </div>
       </div>
-      <a
-        class="mint-searchbar-cancel"
-        @click="visible = false, currentValue = ''"
-        v-show="visible"
-        v-text="cancelText">
-      </a>
+      <div class="btn-cancel" @click="_cancel">
+        <span class="btn-cancel-container">{{cancelText}}</span>
+        <div class="btn-cancel-takeplace">{{cancelText}}</div>
+      </div>
     </div>
-    <div class="mint-search-list" v-show="show || currentValue">
+
+
+    <div class="mint-search-list" v-show="show || hasValue">
       <div class="mint-search-list-warp">
         <slot>
           <x-cell v-for="(item, index) in result" :key="index" :title="item"></x-cell>
@@ -58,7 +66,10 @@ export default {
   data() {
     return {
       visible: false,
-      currentValue: this.value
+      currentValue: this.value,
+      status: this.value ? 'blur' : 'focus',
+      expand: !!this.value,
+      hasValue: !!this.value
     };
   },
 
@@ -67,6 +78,7 @@ export default {
   watch: {
     currentValue(val) {
       this.$emit('input', val);
+      this.hasValue = !!this.currentValue;
     },
 
     value(val) {
@@ -84,11 +96,57 @@ export default {
     placeholder: {
       default: '搜索'
     },
-    result: Array
+    result: Array,
+
+    showCancelBtn: {
+      type: Boolean,
+      default: true
+    },
+    fixedCancelBtn: {
+      type: Boolean,
+      default: false
+    },
+    lang: {
+      type: String,
+      default: 'zh'
+    }
   },
 
   mounted() {
-    this.autofocus && this.$refs.input.focus();
+    setTimeout(()=>{
+      this.autofocus && this.focus();
+    }, 50);
+  },
+
+  methods: {
+    _clear(focus = true) {
+      focus && this.focus();
+      this.currentValue = '';
+      this.hasValue = false;
+    },
+
+    _cancel() {
+      this._clear(false);
+      this.expand = false;
+    },
+
+    _input() {
+      this.hasValue = !!this.currentValue;
+    },
+
+    _blur() {
+      this.status = 'blur';
+      if (this.hasValue) return;
+      this._cancel();
+    },
+
+    // 对外可用的方法
+    focus() {
+      console.log('here');
+      this.status = 'focus';
+      this.expand = true;
+      this.$refs.input.focus();
+    }
   }
 };
 </script>
@@ -97,56 +155,161 @@ export default {
   @import "../../../src/style/var.css";
 
   @component-namespace mint {
-    @component search {
-      height: 100%;
-      height: 100vh;
-      overflow: hidden;
-    }
-
-    @component searchbar {
-      position: relative;
-      align-items: center;
-      background-color: $color-grey;
-      box-sizing: border-box;
-      display: flex;
-      padding: 8px 10px;
-      z-index: 1;
-
-      @descendent inner {
-        align-items: center;
-        background-color: $color-white;
-        border-radius: 2px;
-        display: flex;
-        flex: 1;
-        height: 28px;
-        padding: 4px 6px;
-
-        .mintui-search {
-          font-size: 12px;
-          color: $color-grey;
-        }
-      }
-
-      @descendent core {
-        appearance: none;
-        border: 0;
-        box-sizing: border-box;
-        width: 100%;
-        height: 100%;
-        outline: 0;
-      }
-
-      @descendent cancel {
-        color: $color-blue;
-        margin-left: 10px;
-        text-decoration: none;
-      }
-    }
-
     @component search-list {
       overflow: auto;
       padding-top: 44px;
       position: absolute 0 0 0 0;
     }
+  }
+</style>
+
+<style scoped>
+  .mint-searchbar{
+    position: relative;
+    padding: 8px 10px;
+    display: flex;
+    background-color: #EFEFF4;
+    height: 28px;
+    overflow: hidden;
+    will-change: opacity;
+    box-sizing: initial;
+    z-index: 1;
+  }
+
+  .mint-searchbar *{
+    box-sizing: initial;
+  }
+
+  .mint-searchbar[data-has-value=true] .placeholder{
+    visibility: hidden;
+  }
+
+  .mint-searchbar[data-has-value=true] .btn-clear{
+    opacity: 1;
+  }
+
+  .mint-searchbar[data-status='blur'] .btn-clear{
+    opacity: 0;
+  }
+
+  .mint-searchbar[data-lang='zh'] .btn-cancel-takeplace{
+    letter-spacing: -17px;
+  }
+
+  .mint-searchbar[data-lang='en'] .btn-cancel-takeplace{
+    letter-spacing: -9px;
+  }
+
+
+  .mint-searchbar[data-expand=true] .input-container::before{
+    flex: 0 0 auto;
+  }
+
+  .mint-searchbar[data-expand=true] .text-input-container{
+    width: calc(100% - 38px);
+    left: 38px;
+  }
+
+  .mint-searchbar[data-fixed-cancel=true] .btn-cancel,
+  .mint-searchbar[data-expand=true] .btn-cancel{
+    opacity: 1;
+      margin-left: 0px;
+      transform: translateX(0px);
+  }
+
+  .mint-searchbar[data-fixed-cancel=true][data-lang] .btn-cancel-takeplace,
+  .mint-searchbar[data-expand=true][data-lang] .btn-cancel-takeplace{
+    letter-spacing: unset;
+  }
+
+  .input-container{
+    flex: auto;
+    margin-right: 10px;
+    border-radius: 3.5px;
+    background: white;
+    display: inline-flex;
+    overflow: hidden;
+    position: relative;
+  }
+
+  .input-container::after,
+  .input-container::before{
+    content: '';
+    flex: auto;
+    transition: all 300ms ease;
+  }
+
+  .input-container::before{
+    margin-left: 15px;
+  }
+
+  .btn-cancel{
+    flex: 0 0 auto;
+    color: #26a2ff;
+    margin-left: -10px;
+    transform: translateX(10px);
+    font-size: 17px;
+    line-height: 1.6;
+  }
+
+  .btn-cancel-container{
+    position: absolute;
+    width: 100vw;
+    z-index: -1;
+  }
+
+  .btn-cancel-takeplace, .btn-cancel{
+    opacity: 0;
+    transition: all 300ms ease;
+    transition-delay: 80ms;
+  }
+
+  .placeholder-container{
+    flex: 0 0 auto;
+    transition: all 300ms ease;
+    display: inline-flex;
+    align-items: center;
+    color: #b2b2b2;
+  }
+
+  .placeholder{
+    font-size: 15px;
+  }
+
+  .search-icon{
+    margin-right: 8px;
+    font-size: 15px;
+  }
+
+  .text-input-container{
+    position: absolute;
+    left: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    display: flex;
+    align-items: center;
+  }
+
+  .text-input{
+    background: transparent;
+    outline: none;
+    border: none;
+    height: 28px;
+    display: block;
+    font-size: 15px;
+    flex: auto;
+    margin-right: 5px;
+  }
+
+  .btn-clear{
+    flex: 0 0 auto;
+    margin-right: 5px;
+    z-index: 0;
+    width: 15px;
+    height: 15px;
+    overflow: hidden;
+    opacity: 0;
+    background-image: url("data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 28 28'%3E%3Ccircle cx='14' cy='14' r='14' fill='%23ccc'/%3E%3Cpath stroke='%23fff' stroke-width='2' stroke-miterlimit='10' d='M8 8l12 12'/%3E%3Cpath fill='none' stroke='%23fff' stroke-width='2' stroke-miterlimit='10' d='M20 8L8 20'/%3E%3C/svg%3E");
   }
 </style>

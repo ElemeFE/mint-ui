fdccacaca18ba28e58f67690a4a82723377f7fe2
cloudIC <844033231@qq.com>
2017-12-11 15:31:11 +0800

    [build] 2.2.13

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ import PaletteButton from '../packages/palette-button';
 import '../src/assets/font/iconfont.css';
 import merge from './utils/merge';
 
-const version = '2.2.12';
+const version = '2.2.13';
 const install = function(Vue, config = {}) {
   if (install.installed) return;
 

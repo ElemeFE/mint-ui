a60ff92e8ca0deace5bb454d79dbd1ba52985ddd
yoyo837 <yoyo837@hotmail.com>
2018-02-06 18:02:19 +0800

    v-for add :key

--- packages/picker/src/picker-slot.vue
+++ packages/picker/src/picker-slot.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="picker-slot" :class="classNames" :style="flexStyle">
     <div v-if="!divider" ref="wrapper" class="picker-slot-wrapper" :class="{ dragging: dragging }" :style="{ height: contentHeight + 'px' }">
-      <div class="picker-item" v-for="itemValue in mutatingValues" :class="{ 'picker-selected': itemValue === currentValue }" :style="{ height: itemHeight + 'px', lineHeight: itemHeight + 'px' }">
+      <div class="picker-item" v-for="itemValue in mutatingValues" :key="itemValue[valueKey] || itemValue" :class="{ 'picker-selected': itemValue === currentValue }" :style="{ height: itemHeight + 'px', lineHeight: itemHeight + 'px' }">
         {{ typeof itemValue === 'object' && itemValue[valueKey] ? itemValue[valueKey] : itemValue }}
       </div>
     </div>

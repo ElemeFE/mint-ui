500025b1bf5db6c9a484740ad4c638d95315932c
iiijarvis <ijarvis@163.com>
2019-06-22 15:59:54 +0800

    fixed:loadmore 目标滚动dom元素不一定需要overflowY

--- packages/loadmore/src/loadmore.vue
+++ packages/loadmore/src/loadmore.vue
@@ -204,14 +204,10 @@
 
       getScrollEventTarget(element) {
         let currentNode = element;
-        while (currentNode && currentNode.tagName !== 'HTML' &&
+        if (currentNode && currentNode.tagName !== 'HTML' &&
           currentNode.tagName !== 'BODY' && currentNode.nodeType === 1) {
-          let overflowY = document.defaultView.getComputedStyle(currentNode).overflowY;
-          if (overflowY === 'scroll' || overflowY === 'auto') {
             return currentNode;
           }
-          currentNode = currentNode.parentNode;
-        }
         return window;
       },
 
